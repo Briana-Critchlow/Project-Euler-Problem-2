4af390072b4cf75d9eae1a08b732f878f94d4220
Briana Sullivan <briana.m.sullivan.18@gmail.com>
2021-06-11 15:05:19 -0700

    PE2V1 final solution

--- PE2V1.js
+++ PE2V1.js
@@ -1,4 +1,4 @@
-let array1 = [0, 1, 2];
+
 
 /*for (let i = 0; i < array1.length; i++) {
     let counter = () => {
@@ -9,17 +9,25 @@ let array1 = [0, 1, 2];
 console.log(array1);*/
 
 
-function addDigit(arr) {
+/*function addDigit(arr) {
     arr.push(array1[array1.length - 1] + array1[array1.length - 2]);
-}
+}*/
 
+let array1 = [1, 2];
 
-for (let i = 0; i < array1.length; i++) {
-    if ((array1.length - 1) < 10) {
+for (let i = 1; i <= array1.length; i++) {
+    function addDigit(arr) {
+        arr.push(array1[array1.length - 1] + array1[array1.length - 2]);
+    } if ((array1[array1.length - 1]) < 4000000) {
         addDigit(array1);
     } else {
         break;
     }
 }
 
-console.log(array1);
+let evenNums = array1.filter(num => num % 2 === 0);
+let sum = evenNums.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+});
+
+console.log(sum);
