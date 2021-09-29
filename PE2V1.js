6f1dda9d64bf9c2a6c48ea16aec8328ca8c2530e
Briana Sullivan <briana.m.sullivan.18@gmail.com>
2021-09-29 09:46:13 -0700

    Update PE2V1.js

--- PE2V1.js
+++ PE2V1.js
@@ -1,18 +1,3 @@
-
-
-/*for (let i = 0; i < array1.length; i++) {
-    let counter = () => {
-        let addLastTwo = (array1[array1.length - 1] + array1[array1.length - 2]);
-    }
-    let addBackIn = () => array1.push(addLastTwo);
-}
-console.log(array1);*/
-
-
-/*function addDigit(arr) {
-    arr.push(array1[array1.length - 1] + array1[array1.length - 2]);
-}*/
-
 let array1 = [1, 2];
 
 for (let i = 1; i <= array1.length; i++) {
